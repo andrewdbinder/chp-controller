09bed966f2ed7e16e7387815930049415578de39
Andrew Binder <andrewdbinder@gmail.com>
2021-01-28 15:19:01 -0800

    update titlebar color based on run environment

--- src/index.tsx
+++ src/index.tsx
@@ -8,27 +8,33 @@ import icon from '../assets/icon.svg';
 
 const customTitlebar = require('custom-electron-titlebar');
 
-// TODO: implement different color titlebar for unpackaged mode
-// const RESOURCES_PATH = app.isPackaged
-//   ? path.join(process.resourcesPath, 'resources')
-//   : path.join(__dirname, '../assets');
-//
-// const getAssetPath = (...paths: string[]): string => {
-//   return path.join(RESOURCES_PATH, ...paths);
-// };
+// implement different color titlebar for unpackaged mode
+
+let backgroundColor: string;
+if (
+  process.env.NODE_ENV === 'development' ||
+  process.env.DEBUG_PROD === 'true'
+) {
+  backgroundColor = customTitlebar.Color.fromHex('#ffc107');
+} else {
+  backgroundColor = customTitlebar.Color.fromHex('#343a40');
+}
 
 window.addEventListener('DOMContentLoaded', () => {
+  console.log(process.env.NODE_ENV);
+  // eslint-disable-next-line no-new
   new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#343a40'),
+    backgroundColor,
     icon,
     unfocusEffect: false,
   });
 
-  const replaceText = (selector, text) => {
+  const replaceText = (selector: any, text: any) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
   };
 
+  // eslint-disable-next-line no-restricted-syntax
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type]);
   }
